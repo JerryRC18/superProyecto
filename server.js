370ba68088d44ac588a27243a057894bf215f25b
Gerardo <jesusgrobles9@gmail.com>
2022-10-04 11:25:10 -0500

    Configuración de server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+ //Backend
+ //Importar dependencias
+ 
+ import express from 'express'
+ import bcrypt from 'bcrypt'
+ import Stripe from 'stripe'
+ import { initializeApp } from 'firebase/app'
+ 
+ //Configuracion de firebase
+
+ const firebaseConfig = {
+
+ }
+
+ 
