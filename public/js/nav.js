5b9a249892528aa99f1e378a1cf736cb0503b501
Gerardo <jesusgrobles9@gmail.com>
2022-10-19 13:33:18 -0500

    Add css code, fixed bugs in nav.js and add sections in index.html

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,37 +1,56 @@
 const navbar = document.querySelector('.navbar')
 
-window.addEventListener('scroll', () =>{
-    console.log('scroll', scrollY)
-    if(scrollY => 180){
+window.addEventListener('scroll', () => {
+    console.log('scroll',scrollY)
+    if(scrollY>=180){
         navbar.classList.add('bg')
-    }else {
+    }else{
         navbar.classList.remove('bg')
     }
 })
 
-const createNavbar = () =>{
-    navbar.innerHTML += 
+const createNavbar=()=>{
+    navbar.innerHTML+=
     `
     <ul class="links-container">
         <li class="link-item">
             <a href="#" class="link active">Home</a>
         </li>
         <li class="link-item">
-            <a href="#" class="link">Product</a>
+            <a href="#" class="link active">Product</a>
         </li>
         <li class="link-item">
-            <a href="#" class="link">About</a>
+            <a href="#" class="link active">About</a>
         </li>
         <li class="link-item">
-            <a href="#" class="link">Contact</a>
+            <a href="#" class="link active">Contact</a>
         </li>
     </ul>
+    <div class="user-interactions">
+        <div class="search-box">
+            <input type="text" class="search" placeholder="search item">
+            <button class="search-btn">
+            <img src="./img/search.png" class="cart-icon">
+            </button>
+        </div>
+        <div class="cart" onclick="location.href='/cart'">
+            <img src="./img/cart.png" alt="" class="cart-icon">
+            <span class="cart-item-count">00</span>
+        </div>
+        <div class="user">
+            <img src="./img/user.png" alt="" class="user-icon">
+        <div class="user-icon-popup">
+            <p>Login to your account</p>
+            <p>Login</p>
+        </div>
+        </div>
+    </div>
     `
 }
 
 createNavbar()
 
-//User icon popup
+//user icon popup
 let userIcon = document.querySelector('.user-icon')
 let userPopupIcon = document.querySelector('.user-icon-popup')
 
