be72f34dc2b5a9078e4884a4f35434b6e3227f9f
Gerardo <jesusgrobles9@gmail.com>
2022-11-11 11:34:30 -0600

    add seller

--- public/js/seller.js
+++ public/js/seller.js
@@ -1,8 +1,8 @@
 window.onload = () => {
     let user = JSON.parse(sessionStorage.user || null)
-    if(user === null){
+    if(user === null) {
         location.replace('/login')
-    }else if( user.seller ){
+    } else if (user.seller) {
         location.replace('/dashboard')
     }
 }
@@ -10,23 +10,30 @@ window.onload = () => {
 let loader = document.querySelector('.loader')
 let applyBtn = document.querySelector('.apply-btn')
 
-applyBtn.addEventListener('click', () => {
+applyBtn.addEventListener('click', async () => {
+    console.log('click')
     let businessName = document.querySelector('#name').value
     let address = document.querySelector('#address').value
     let about = document.querySelector('#about').value
     let number = document.querySelector('#number').value
 
-    if(!businessName.lenght || !address.lenght || !about.lenght || number.lenght < 10 || Number(number)){
-        showFormError("some info is invalid")
-    } else{
-        //Enviar datos
-        loader.style.display = 'block'
-        sendData('seller', {
-            name: businessName,
-            address,
-            about,
-            number, 
-            email: JSON.parse(sessionStorage.user).email
-        })
-    }
+    if(!businessName.length || !address.length 
+        || !about.length || number.length < 10
+        || !Number(number)){
+            showFormError('some info is invalid')
+        } else {
+            // enviar datos
+            loader.style.display = 'block'
+            await sendData('seller', {
+                name: businessName,
+                address,
+                about,
+                number,
+                email: JSON.parse(sessionStorage.user).email
+            })
+            const Usuario = JSON.parse(sessionStorage.getItem('user'))
+            Usuario.seller = true
+            sessionStorage.setItem('user', JSON.stringify(Usuario));
+            location.replace('/')
+        }
 })
